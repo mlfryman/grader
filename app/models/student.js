0c384f37f8820e5d249677e298217153c6049ed7
Melanie Fryman <melanie@frymanet.com>
2014-08-05 15:57:33 -0500

    finished student model

--- app/models/student.js
+++ app/models/student.js
@@ -3,66 +3,87 @@
 var _ = require('lodash');
 var Mongo = require('mongodb');
 
-function Student(o){
-  this.name = o.name;
-  this.color = o.color;
-  this.avg = 0;
-  this._isSuspended = false;
-  this._isHonor = false;
+function Student(obj){
+  this.name = obj.name;
+  this.color = obj.color;
   this.tests = [];
-  this.fails = 0;
+  this._suspended = {suspended:'no', color:'green'};
+  this._honorRoll = {honor:'no', color:'red'};
 }
 
 Object.defineProperty(Student, 'collection', {
   get: function(){return global.mongodb.collection('students');}
 });
 
+Student.prototype.avg = function(){
+  if(!this.tests.length){return 0;}
+
+  var sum = this.tests.reduce(function(a,b){return a + b;});
+  return sum/this.tests.length;
+
+};
+
+Student.prototype.letter = function(){
+  var avg = this.avg();
+  if(avg < 60){
+    return 'F';
+  }else if(avg < 70){
+    return 'D';
+  }else if(avg < 80){
+    return 'C';
+  }else if(avg < 90){
+    return 'B';
+  }else{
+    return 'A';
+  }
+};
+
+Student.prototype.update = function(){
+  if(!this.tests.length){return;}
+
+  if(this.avg() > 95){
+    this._honorRoll = {honor:'yes', color:'green'};
+  }
+
+  var count = 0;
+  for(var i = 0; i < this.tests.length; i++){
+    count += (this.tests[i] < 60) ? 1 : 0;
+    if(count >= 3){
+      this._suspended = {suspended:'yes', color:'red'};
+      break;
+    }
+  }
+};
+
+Student.prototype.addTest = function(obj){
+  var score = parseFloat(obj.score);
+  this.tests.push(score);
+  this.update();
+};
+
 Student.prototype.save = function(cb){
   Student.collection.save(this, cb);
 };
 
-Student.all = function(cb){
+Student.find = function(cb){
   Student.collection.find().toArray(function(err, objects){
-    var students = objects.map(function(o){
-      return changePrototype(o);
-    });
-
+    var students = objects.map(function(o){return reProto(o);});
     cb(students);
   });
 };
 
 Student.findById = function(id, cb){
-  var _id = Mongo.ObjectID(id);
+  id = Mongo.ObjectID(id);
 
-  Student.collection.findOne({_id:_id}, function(err, obj){
-    var Student = changePrototype(obj);
-
-    cb(Student);
+  Student.collection.findOne({_id:id}, function(err, obj){
+    var student = reProto(obj);
+    cb(student);
   });
 };
 
-Student.deleteById = function(id, cb){
-  var _id = Mongo.ObjectID(id);
-
-  Student.collection.findAndRemove({_id:_id}, cb);
-};
-
-Student.prototype.addTest = function(student){
-  if(this._isSuspended){return 0;}
-
-  if(student.fails >= 3 && !student._isSuspended){
-    student._isSuspended = true;
-    this.fails++;
-  }
-};
-
-
-
 module.exports = Student;
 
-// PRIVATE FUNCTIONS ///
-
-function changePrototype(obj){
-  var student = _.create(Student.prototype, obj);
-  return student;
+// Helper Functions
+function reProto(obj){
+  return _.create(Student.prototype, obj);
 }
--- test/unit/student.js
+++ test/unit/student.js
@@ -9,6 +9,7 @@ var dbConnect = require('../../app/lib/mongodb');
 var Mongo = require('mongodb');
 
 var s1, s2, s3;
+var sObj1 = {name:'Wesley Crusher', color:'pink'};
 
 describe('Student', function(){
   before(function(done){
@@ -19,13 +20,9 @@ describe('Student', function(){
 
   beforeEach(function(done){
     Student.collection.remove(function(){
-      var o1 = {name:'Wesley Crusher', color:'pink'};
-      var o2 = {name:'Diana Troy', color:'blue'};
-      var o3 = {name:'Jean Luc Picard', color:'red'};
-
-      s1 = new Student(o1);
-      s2 = new Student(o2);
-      s3 = new Student(o3);
+      s1 = new Student(sObj1);
+      s2 = new Student({name:'Diana Troy', color:'blue'});
+      s3 = new Student ({name:'Jean Luc Picard', color:'red'});
 
       s1.save(function(){
        s2.save(function(){
@@ -38,65 +35,87 @@ describe('Student', function(){
   });
 
   describe('constructor', function(){
-    it('should create a new Student object', function(){
-      var o = {name:'Wesley Crusher', color:'pink'};
-      var wesley = new Student(o);
+    it('should create a student with proper attributes', function(){
+      s1 = new Student(sObj1);
+      expect(s1.name).to.equal('Wesley Crusher');
+      expect(s1.color).to.equal('pink');
+      expect(s1.tests).to.have.length(0);
+      expect(s1).to.be.instanceof(Student);
+      expect(s1._suspended).to.eql({suspended:'no', color:'green'});
+      expect(s1._honorRoll).to.eql({honor:'no', color:'red'});
+    });
+  });
 
-      expect(wesley).to.be.instanceof(Student);
-      expect(wesley.name).to.equal('Wesley Crusher');
-      expect(wesley.color).to.equal('pink');
-      expect(wesley._isSuspended).to.be.false;
-      expect(wesley._isHonor).to.be.false;
-      expect(wesley.tests).to.have.length(0);
-      expect(wesley.fails).to.be.equal(0);
+  describe('#avg', function(){
+    it('should return the average of all test scores', function(){
+      s1 = new Student(sObj1);
+      s1.tests = [95, 85, 75, 65, 55];
+      expect(s1.avg()).to.be.closeTo(75, 0.1);
+      s1.tests = [];
+      expect(s1.avg()).to.equal(0);
+    });
+  });
+
+  describe('#letter', function(){
+    it('should return a letter grade vased on #avg', function(){
+      s1 = new Student(sObj1);
+      s1.tests = [95, 85, 75, 65, 55];
+      expect(s1.letter()).to.equal('C');
+    });
+  });
+
+  describe('#update', function(){
+    it('should update _honor based on scores', function(){
+      s1 = new Student(sObj1);
+      s1.tests = [96];
+      s1.update();
+      expect(s1._honorRoll).to.eql({honor:'yes', color:'green'});
+    });
+
+    it('should suspend a student based on fails', function(){
+      s1 = new Student(sObj1);
+      s1.tests = [34,16,45,72];
+      s1.update();
+      expect(s1._suspended).to.eql({ suspended:'yes', color:'red'});
+    });
+  });
+
+  describe('#addTest', function(){
+    it('should add test to student\'s tests array', function(){
+      s1 = new Student(sObj1);
+      s1.addTest({score: '67.1'});
+      expect(s1.tests).to.have.length(1);
+      expect(typeof s1.tests[0]).to.equal('number');
     });
   });
 
   describe('#save', function(){
     it('should save a student to the database', function(done){
-      var o = {name:'Wesley Crusher', color:'pink'};
-      var wesley = new Student(o);
-
-      wesley.save(function(){
-        expect(wesley._id).to.be.instanceof(Mongo.ObjectID);
+      s1 = new Student(sObj1);
+      s1.save(function(){
+        expect(s1._id).to.be.instanceof(Mongo.ObjectID);
         done();
       });
     });
   });
 
-  describe('.all', function(){
-    it('should get all students from database', function(done){
-      Student.all(function(students){
+  describe('.find', function(){
+    it('should return an array of all the students', function(done){
+      Student.find(function(students){
         expect(students).to.have.length(3);
-        expect(students[0]).to.respondTo('addTest');
+        expect(students[0]).to.respondTo('avg');
         done();
       });
     });
   });
 
   describe('.findById', function(){
-    it('should find a student by its id', function(done){
-      Student.findById(s1._id.toString(), function(student){
-        expect(student.name).to.equal('Wesley Crusher');
-        expect(student).to.respondTo('addTest');
+    it('should find one student from an id string', function(done){
+      Student.findById(s2._id.toString(), function(student){
+        expect(student).to.eql(s2);
         done();
       });
     });
   });
-
-  describe('#addTest', function(){
-    it('should add a new test to student\'s test array', function(){
-      var o = {name:'Wesley Crusher', color:'pink'};
-      var wesley = new Student(o);
-      
-      wesley.addTest();
-
-      expect(wesley.tests).to.have.length(1);
-      expect(wesley._isSuspended).to.be.false;
-    });
-  });
-
-
-
 
 });
