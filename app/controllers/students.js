b60ef5ec7e7a52d24b03aa4ce79ff3d3bc074eff
Melanie Fryman <melanie@frymanet.com>
2014-08-06 03:21:40 -0500

    almost complete; cannot GET or POST Ss

--- app/controllers/students.js
+++ app/controllers/students.js
@@ -4,6 +4,7 @@ var Student = require('../models/student');
 
 exports.index = function(req, res){
   Student.find(function(students){
+    console.log(students);
     res.render('students/index', {students:students});
   });
 };
@@ -15,6 +16,28 @@ exports.init = function(req, res){
 exports.create = function(req, res){
   var s1 = new Student(req.body);
   s1.save(function(){
-    res.redirect('students/index');
+    res.redirect('/students');
+  });
+};
+
+exports.show = function(req, res){
+  Student.findById(req.params.id, function(student){
+    res.render('students/show', {student:student});
+  });
+};
+
+exports.addTest = function(req, res){
+  Student.findById(req.params.id, function(student){
+    res.render('students/addtest', {student:student});
+  });
+};
+
+exports.update = function(req, res){
+  var score = req.body;
+  Student.findById(req.params.id, function(student){
+    student.addTest(score);
+    student.save(function(){
+      res.redirect('/students/' + req.params.id);
+    });
   });
 };
--- app/lib/pipeline.js
+++ app/lib/pipeline.js
@@ -2,6 +2,7 @@
 
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
+var methodOverride = require('express-method-override');
 var home = require('../controllers/home');
 var students = require('../controllers/students');
 
@@ -9,6 +10,7 @@ module.exports = function(app, express){
   app.use(morgan('dev'));
   app.use(express.static(__dirname + '/../static'));
   app.use(bodyParser.urlencoded({extended:true}));
+  app.use(methodOverride());
 
   app.get('/', home.index);
   app.get('/about', home.about);
@@ -17,6 +19,10 @@ module.exports = function(app, express){
 
   app.get('/students/new', students.init);
   app.post('/students/new', students.create);
+  app.get('/students/index', students.index);
+  app.get('/students/:id', students.show);
+  app.get('/students/:id/test', students.init);
+  app.post('/students/:id/test', students.update);
 
   console.log('Pipeline Configured');
 };
